e9504f59a168af25d291c5c8d962c3c5adf3ec35
Kris Mulyandani Putra <kris.mp7777@gmail.com>
2013-10-09 15:51:38 +0700

    ganti konten

--- application/views/daftar.php
+++ application/views/daftar.php
@@ -3,9 +3,9 @@
 	<div id="wrapper">
 		<h1>Form Pendaftaran Bakti Pemuda</h1>
 
-		<p>Bakti Pemuda digagas, dibentuk dan diluncurkan untuk mempercepat peningkatan ekonomi masyarakat secara mandiri dan berkelanjutan di wilayah perbatasan, khususnya di daerah tertinggal. Program ini bertujuan untuk meningkatkan partisipasi masyarakat meningkatkan pelaksanaan program-program pembangunan oleh pemerintah yang berpihak kepada masyarakat miskin (pro-poor),meningkatkan modal sosial masyarakat dan peningkatan inovasi dan pemanfaatan teknologi tepat guna dalam pembangunan ekonomi masyarakat.</p>
+		<p>Kami percaya bahwa pemuda indonesia haruslah menjadi sosok yang keren. Bagi pemuda, keren bukanlah pilihan, melainkan sebuah keharusan. Dan keren itu diukur bukan seberapa bagus tampilanmu atau bahkan seberapa bagus nilai-nilaimu. Yang lebih penting lagi, keren itu bukan seberapa pintar kamu mengkritisi dunia sekitarmu, lingkunganmu, pemerintahanmu, dan negaramu. Keren itu adalah tidak hanya sekedar mengkritik tetapi juga berani untuk melakukan sesuatu untuk apa yang kamu kritisi, walau itu kecil.</p>
 
-		<p>Untuk itu Bakti pemuda mengundang Pemuda-pemudi Indonesia usia 20 s.d. 30 tahun, sehat jasmani dan rohani, sarjana berbagai disiplin ilmu dengan pengalaman kerja minimal 2 tahun serta memiliki pengalaman organisasi baik di dalam sekolah/kampus maupun diluar dan memiliki kompentensi tertentu sesuai dengan kondisi daerah penempatan untuk bergabung.</p>
+		<p>Untuk jadi pemuda yang keren membutuhkan keberanian dan kerja keras. Bakti pemuda mengajak kamu untuk menunjukkan seberapa keren dirimu melalui partisipasi sebagai fasilitator bakti pemuda. Daftarkan dirimu sekarang dan tunjukan kepada mereka bahwa kamu memang benar-benar keren. Merdeka.</p>
 
 		<p class="italic">Silahkan isi form dengan mengklik tombol <strong>"Daftar"</strong> di samping kanan dan tunjukkan baktimu!</p>
 	</div>
