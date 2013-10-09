5d652d961f2a8b1103b224a421e221056df62267
Kris Mulyandani Putra <kris.mp7777@gmail.com>
2013-10-09 17:55:02 +0700

    This reverts commit ca6d5199ea7c1c4c7ffed6938dfc79583e7ce40c.

--- application/controllers/daftar.php
+++ application/controllers/daftar.php
@@ -2,22 +2,12 @@
 
 class Daftar extends CI_Controller {
 
-	function __construct() {
-		parent::__construct();
-		$this->load->model('daftar_model');
-	}
-
 	public function index()
 	{
 		// title
 		$data['meta_title'] = 'Pendaftaran';
 		
 		$this->load->view('daftar', $data);
-	}
-
-	function add()
-	{
-		
 	}
 }
 
--- application/views/_form/register.php
+++ application/views/_form/register.php
@@ -3,23 +3,23 @@
 		Thank you.  We'hv received your feedback.
 	</div>
 	<div id="mrova-form">
-		<form id="mrova-contactform" action="<?php echo site_url('daftar/add') ?>" method="post">
+		<form id="mrova-contactform" action="#" method="post">
 			<ul >
 				<li>
 					<label for="mrova-name">Nama*</label>
-					<input type="text" name="mrova-name" class="validate[required]" id="mrova-name" value="">
+					<input type="text" name="mrova-name" class="required" id="mrova-name" value="">
 				</li>
 				<li>
 					<label for="mrova-email">Email*</label>
-					<input type="text" name="mrova-email" class="validate[required]" id="mrova-email" value="">
+					<input type="text" name="mrova-email" class="required" id="mrova-email" value="">
 				</li>
 				<li>
 					<label for="mrova-message">Motivasi menjadi fasilitator bakti pemuda*</label>
-					<textarea class="validate[required]" id="mrova-message" name="mrova-message"  rows="8" cols="30"></textarea>
+					<textarea class="required" id="mrova-message" name="mrova-message"  rows="8" cols="30"></textarea>
 				</li>
 				<li>
 					<label for="mrova-cv">Upload CV*</label>
-					<input type="file" name="mrova-cv" class="validate[required]" id="mrova-cv" value="">
+					<input type="file" name="mrova-cv" class="required" id="mrova-cv" value="">
 				</li>
 			</ul>
 			<input type="submit" value="Kirim" id="mrova-sendbutton" name="mrova-sendbutton">
